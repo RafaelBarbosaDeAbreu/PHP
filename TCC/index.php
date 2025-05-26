846f7e8c0a9e509eb91c61022a54fd1808957ae6
RafaelBarbosaDeAbreu <rafaelbarbosamm@gmail.com>
2025-05-26 20:10:11 -0300

    Main page of website

---
+++ TCC/index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>Busca de professores</title>
+    <style>
+body {
+            
+    text-align: center;
+    font-family: monospace;
+    font-style: oblique;
+    background-color: blue;
+}
+
+header {
+    padding: 20px;
+    background-color: #ffa500;
+    color: #fff;
+}
+
+h1 {
+    font-size: 37px;
+    margin: 0;
+}
+
+.button {
+    display: inline-block;
+    padding: 20px 30px;
+    margin: 15px;
+    background-color: #ffa500;
+    border: none;
+    border-radius: 5px;
+    color: #fff;
+    font-size: 46px;
+    cursor: pointer;
+    text-decoration: none;
+    width: 300px;
+    font-family: monospace;
+    font-style: oblique;
+}
+
+.button:hover {
+    background-color: #555;
+}
+        
+    </style>
+</head>
+<body>
+    <header>
+        <h1 style="font-size: 90px">Bem vindo à escola Dr. Solon Tavares</h1>
+    </header>
+    <br><br><br><br><br><br><br><br><br><br>
+    <p style="font-size: 60px; color: white">O que você deseja fazer agora?</p>
+    <a href="pgTabela.php" class="button" >Pesquisar Professores</a>
+    <a href="pgLogin.php" class="button">Modo Administrador</a>
+</body>
+</html>
